92405135b1641cb068c2dd7cd0ea569b555f5f44
Wangxs404 <wangxs1995s@gmail.com>
2025-09-07 20:37:43 +0800

    build: 将基础路径从'/wangxs404.github.io/'改为'/'

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { traeBadgePlugin } from 'vite-plugin-trae-solo-badge';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/wangxs404.github.io/',
+  base: '/',
   build: {
     sourcemap: 'hidden',
   },
